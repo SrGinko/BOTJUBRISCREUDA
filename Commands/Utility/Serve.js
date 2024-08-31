c572fba5e4d7b71ac9318f1cc6dfa78799516eff
Pedro Monteiro <pedeohenriquecardoso@gmail.com>
2024-08-31 19:21:57 -0300

    V1.2

--- Commands/Utility/Serve.js
+++ Commands/Utility/Serve.js
@@ -1,15 +1,15 @@
 const { SlashCommandBuilder, EmbedBuilder, ButtonBuilder, ActionRowBuilder, ButtonStyle } = require("discord.js")
 
 const Server1 = new EmbedBuilder()
-    .setTitle('ManicomioServer (Aternos Fechado)')
+    .setTitle('Manicomio')
     .setColor('Green')
-    .setDescription('Servidor Vanila de sobrevivencia Pelo Aternos')
-    .setThumbnail('https://i0.wp.com/www.alphr.com/wp-content/uploads/2022/06/Aternos.png?resize=508%2C507&ssl=1')
+    .setDescription('Servidor com mod DownCraft')
+    .setThumbnail('https://static.wikia.nocookie.net/dawncraft/images/3/39/Site-community-image/revision/latest?cb=20230312224812')
     .addFields(
-        { name: 'Endereço do Servidor', value: 'ManicomioServer0.aternos.me', inline: true },
-        { name: 'Porta', value: '32462', inline: true }
+        { name: 'Endereço do Servidor', value: 'selene.lura.pro:25570', inline: true },
+        { name: 'Porta', value: '25570', inline: true }
     )
-    .setImage('https://cdn.discordapp.com/attachments/1031036409231986778/1209664881146462278/efbda851-d764-455c-abd4-281712ed3a2e.png?ex=65f0f9b6&is=65de84b6&hm=0c1804ba1e1c1569ec895d30d8b725c433d7a343224a67fc10524d13bf800138&')
+    .setImage('https://static.wikia.nocookie.net/dawncraft/images/2/2f/Dawncraft.png/revision/latest/scale-to-width-down/1280?cb=20230916211914')
 
 
 const realms = new EmbedBuilder()
@@ -20,7 +20,7 @@ const realms = new EmbedBuilder()
     .setImage('https://cdn.discordapp.com/attachments/1031036409231986778/1209664881146462278/efbda851-d764-455c-abd4-281712ed3a2e.png?ex=65f0f9b6&is=65de84b6&hm=0c1804ba1e1c1569ec895d30d8b725c433d7a343224a67fc10524d13bf800138&')
 module.exports = {
     data: new SlashCommandBuilder()
-        .setName('realms')
+        .setName('server')
         .setDescription('Realms do Minecraft Bedrock'),
 
     async execute(interaction) {
@@ -33,7 +33,7 @@ module.exports = {
         const row1 = new ActionRowBuilder()
             .addComponents(buttRealms)
 
-        await interaction.reply({ embeds: [realms], components: [row1] })
+        await interaction.reply({ embeds: [Server1] })
 
     }
 }
