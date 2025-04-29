f46ce62f0c69076bbd68f113e4dcb8a668c96e1c
SrGinko <91520452+SrGinko@users.noreply.github.com>
2025-04-29 12:54:15 -0300

    add: adicionando funcionalidade de adicionar um cargo quando impulsiona o servidor

--- Events/GuildMemberUpdate.js
+++ Events/GuildMemberUpdate.js
@@ -4,10 +4,12 @@ module.exports = {
   name: Events.GuildMemberUpdate,
   execute(oldMember, newMember) {
     const boosterRole = newMember.guild.roles.cache.find(role => role.name === 'Server Booster');
+    const burges = newMember.guild.roles.cache.find(role => role.name === 'Burgês');
     if (!boosterRole) return;
 
     if (!oldMember.roles.cache.has(boosterRole.id) && newMember.roles.cache.has(boosterRole.id)) {
         newMember.guild.channels.cache.get('1031036295482454069').send(`Obrigada ${newMember} por impulsionar o servidor! :heart:`);
+        newMember.roles.add(burges.id).catch(console.error);
     }
   },
 };
