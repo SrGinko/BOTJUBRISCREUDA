c53a364206c1c7e795aed2ea2862cabc025ca07b
SrGinko <pedeohenriquecardoso@gmail.com>
2025-07-14 00:48:46 -0300

    refactor: refatoração do código

--- Controller.js
+++ Controller.js
@@ -3,6 +3,7 @@ const dotenv = require('dotenv')
 dotenv.config()
 const { RAWG_API, URL_USUARIO } = process.env
 const axios = require('axios')
+const { getRandonCores } = require('./Utils/Cores')
 
 const embed = new EmbedBuilder()
 
@@ -51,8 +52,10 @@ async function controler(interaction) {
                 const response = await Buscarjogo(game)
                 const jogo = response[0]
                 try {
+                    const cor = getRandonCores()
+
                     const container = new ContainerBuilder({
-                        accent_color: 0x3c1099,
+                        accent_color: cor,
                         timestamp: true,
                         components: [
                             new TextDisplayBuilder({
@@ -131,122 +134,41 @@ async function controler(interaction) {
             case 'alterbanner': {
                 const alterarBanner = interaction.values[0]
 
-                switch (alterarBanner) {
-                    case '0':
-                        alterarBanner(userId, select)
-                        embed.setDescription('Banner Alterado com sucesso')
-                        embed.setColor('Green')
-                        addXp(userId, 5)
-                        return await interaction.reply({ embeds: [embed], ephemeral: true })
-                        break;
-                    case '1':
-                        alterarBanner(userId, select)
-                        embed.setDescription('Banner Alterado com sucesso')
-                        embed.setColor('Green')
-                        addXp(userId, 5)
-                        return await interaction.reply({ embeds: [embed], ephemeral: true })
-                        break;
-                    case '2':
-                        alterarBanner(userId, select)
-                        embed.setDescription('Banner Alterado com sucesso')
-                        embed.setColor('Green')
-                        addXp(userId, 5)
-                        return await interaction.reply({ embeds: [embed], ephemeral: true })
-                        break;
-                    case '3':
-                        alterarBanner(userId, select)
-                        embed.setDescription('Banner Alterado com sucesso')
-                        embed.setColor('Green')
-                        addXp(userId, 5)
-                        return await interaction.reply({ embeds: [embed], ephemeral: true })
-                        break;
-                    case '4':
-                        alterarBanner(userId, select)
-                        embed.setDescription('Banner Alterado com sucesso')
-                        embed.setColor('Green')
-                        addXp(userId, 5)
-                        return await interaction.reply({ embeds: [embed], ephemeral: true })
-                        break;
-                    case '5':
-                        alterarBanner(userId, select)
-                        embed.setDescription('Banner Alterado com sucesso')
-                        embed.setColor('Green')
-                        addXp(userId, 5)
-                        return await interaction.reply({ embeds: [embed], ephemeral: true })
-                        break;
-                    case '6':
-                        alterarBanner(userId, select)
-                        embed.setDescription('Banner Alterado com sucesso')
-                        embed.setColor('Green')
-                        addXp(userId, 5)
-                        return await interaction.reply({ embeds: [embed], ephemeral: true })
-                        break;
-                    case '7':
-                        alterarBanner(userId, select)
-                        embed.setDescription('Banner Alterado com sucesso')
-                        embed.setColor('Green')
-                        addXp(userId, 5)
-                        return await interaction.reply({ embeds: [embed], ephemeral: true })
-                        break;
-                    case '8':
-                        alterarBanner(userId, select)
-                        embed.setDescription('Banner Alterado com sucesso')
-                        embed.setColor('Green')
-                        addXp(userId, 5)
-                        return await interaction.reply({ embeds: [embed], ephemeral: true })
-                        break;
-                    case '9':
-                        alterarBanner(userId, select)
-                        embed.setDescription('Banner Alterado com sucesso')
-                        embed.setColor('Green')
-                        addXp(userId, 5)
-                        return await interaction.reply({ embeds: [embed], ephemeral: true })
-                        break;
+                try {
+                    await axios.patch(`${URL_USUARIO}/${userId}`, {
+                        wallpaper: +alterarBanner
+                    })
 
-                    case '10':
-                        alterarBanner(userId, select)
-                        embed.setDescription('Banner Alterado com sucesso')
-                        embed.setColor('Green')
-                        addXp(userId, 5)
-                        return await interaction.reply({ embeds: [embed], ephemeral: true })
-                        break;
-                    case '11':
-                        alterarBanner(userId, select)
-                        embed.setDescription('Banner Alterado com sucesso')
-                        embed.setColor('Green')
-                        addXp(userId, 5)
-                        return await interaction.reply({ embeds: [embed], ephemeral: true })
-                        break;
-
-                    case '12':
-                        alterarBanner(userId, select)
-                        embed.setDescription('Banner Alterado com sucesso')
-                        embed.setColor('Green')
-                        addXp(userId, 5)
-                        return await interaction.reply({ embeds: [embed], ephemeral: true })
-                        break;
-
-                    case '13':
-                        alterarBanner(userId, select)
-                        embed.setDescription('Banner Alterado com sucesso')
-                        embed.setColor('Green')
-                        addXp(userId, 5)
-                        return await interaction.reply({ embeds: [embed], ephemeral: true })
-                        break;
-
-                    default:
-                        embed.setDescription('Não foi possível Alterar o Banner')
-                        embed.setColor('Red')
-                        return await interaction.reply({ embeds: [embed], ephemeral: true })
-                        break;
+                } catch (error) {
+                    console.log(error.message + error.response.data)
                 }
 
-            } break
+                const banners = await Banner()
+                
+                const conteiner = new ContainerBuilder({
+                    accent_color: 0x00f521,
+                    timestamp: true,
+                    components: [
+                        new TextDisplayBuilder({
+                            content: `# Banner alterado com sucesso!\n Esse é o novo banner:`,
+                            style: 'Short',
+                        }),
+                        new MediaGalleryBuilder({
+                            items: [
+                                {
+                                    media:{
+                                        type: 'Image',
+                                        url: banners[+alterarBanner].banner
+                                    }
+                                }
+                            ]
+                        })
+                    ]
+                })
 
-
-
+                return await interaction.reply({ flags:[MessageFlags.IsComponentsV2, MessageFlags.Ephemeral], components: [conteiner] })
+            }
         }
-
     } else return
 }
 
@@ -278,19 +200,6 @@ async function Buscarjogo(nameGame) {
         return null;
     }
 }
-
-async function alterarBanner(userId, idBanner) {
-    try {
-        await axios.patch(`${URL_USUARIO}/${userId}`, {
-            wallpaper: +idBanner
-        })
-
-    } catch (error) {
-        console.log(error.message + error.response.data)
-    }
-
-}
-
 
 
 /**
@@ -361,23 +270,23 @@ async function addXp(userId, add) {
 async function Banner() {
 
     const banners = [
-        { name: 'Wave', id: 'wave', banner: 'https://marketplace.canva.com/EAF_ZFGfAwE/1/0/1600w/canva-banner-para-twitch-montanha-vintage-retr%C3%B4-roxo-nqw7QjAVpKo.jpg', cor: '#be81d5' },
-        { name: 'Outono', id: 'outono', banner: 'https://t4.ftcdn.net/jpg/06/45/12/47/360_F_645124745_3CGfuoRYiXRME36HMs4EFvr0qjeejuhV.jpg', cor: '#f74922' },
-        { name: 'Hacker', id: 'hcaker', banner: 'https://wallpapers.com/images/featured/4k-minimalista-2dpumtq7d6vnq2fv.jpg', cor: '#ffffff' },
-        { name: 'Montanhas', id: 'montanhas', banner: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTouyrbJzJZPnrtdcvmjrPmH3hClu7EuJZuJqB0MPJqCWrmCtfJYtQ5cE-rxs76GTaEOxM&usqp=CAU', cor: '#0398ce' },
-        { name: 'Floresta', id: 'floresta', banner: 'https://img.freepik.com/fotos-premium/imagens-de-papel-de-parede-em-4k_655257-1108.jpg', cor: '#6628f6' },
-        { name: 'Astronalta', id: 'astronalta', banner: 'https://res.cloudinary.com/dte7upwcr/image/upload/v1677788739/blog/blog2/ia-criar-imagens/ia-criar-imagens2.jpg', cor: '#01e4ca' },
-        { name: 'Folha', id: 'folha', banner: 'https://img.freepik.com/fotos-gratis/beleza-abstrata-de-outono-em-padrao-multicolorido-de-veios-de-folhas-gerado-por-ia_188544-9871.jpg', cor: '#a20104' },
-        { name: 'Samael', id: 'samael', banner: 'https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/d6d9158f-1b03-4024-9f88-9d599c4c968a/df29tev-80fc62a5-5763-45a3-8b61-ec7f6d703924.png/v1/fit/w_600,h_240,q_70,strp/discord_banner__2__watermarked__by_gothymoth_df29tev-375w-2x.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9MjQwIiwicGF0aCI6IlwvZlwvZDZkOTE1OGYtMWIwMy00MDI0LTlmODgtOWQ1OTljNGM5NjhhXC9kZjI5dGV2LTgwZmM2MmE1LTU3NjMtNDVhMy04YjYxLWVjN2Y2ZDcwMzkyNC5wbmciLCJ3aWR0aCI6Ijw9NjAwIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmltYWdlLm9wZXJhdGlvbnMiXX0.PWzYbhsRZ8zU0Xn4y16vSFFyHg4SgbHE4pEw_O7_-LQ', cor: '#741111' },
-        { name: 'Vaquinha', id: 'vaquinha', banner: 'https://i.pinimg.com/1200x/ad/17/d5/ad17d516ba4254ead5cb9bd2747dcc53.jpg', cor: '#9600db' },
-        { name: 'Imperfection', id: 'imperfection', banner: 'https://i.pinimg.com/originals/95/d0/3c/95d03cf844c7c024347258f8953236dd.gif', cor: '#db00a1' },
-        { name: 'Neblina', id: 'neblina', banner: 'https://images-ext-1.discordapp.net/external/VqkxJ18-8oJKiLMoLUyz46VNBRb1XtCQjrFbJiLfqfo/https/wallpapers.com/images/hd/calm-aesthetic-desktop-8t7o1e3i0gaoodqz.jpg?format=webp&width=1258&height=683', cor: '#1f84ff' },
-        { name: 'LOL', id: 'lol', banner: 'https://www.riotgames.com/darkroom/1440/056b96aab9c107bfb72c1cc818be712a:8e765b8b8b63d537b82096f248c2f169/tf-graves-pride-0.png', cor: '#f27900' },
-        { name: 'Skull Sea Of Thieves', id: 'skull-sea-of-thieves', banner: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRgMl2_YrppPp5AgsNi9DHOjJkCFMDXPC55vQ&s', cor: '#6600ff' },
-        { name: 'Hajime no Ippo', id: 'hajime-no-ippo', banner: 'https://m.media-amazon.com/images/S/pv-target-images/2e1f13308ead2fc251f71910b50e253af2f566d717f64dfcfd69a6ab5d8b00dd._SX1080_FMjpg_.jpg', cor: '#bf2126' },
+        { name: 'Wave', id: '0', banner: 'https://marketplace.canva.com/EAF_ZFGfAwE/1/0/1600w/canva-banner-para-twitch-montanha-vintage-retr%C3%B4-roxo-nqw7QjAVpKo.jpg', cor: '#be81d5', corHEX: 0xbe81d5 },
+        { name: 'Outono', id: '1', banner: 'https://t4.ftcdn.net/jpg/06/45/12/47/360_F_645124745_3CGfuoRYiXRME36HMs4EFvr0qjeejuhV.jpg', cor: '#f74922', corHEX: 0xf74922 },
+        { name: 'Hacker', id: '2', banner: 'https://wallpapers.com/images/featured/4k-minimalista-2dpumtq7d6vnq2fv.jpg', cor: '#ffffff', corHEX: 0xffffff },
+        { name: 'Montanhas', id: '3', banner: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTouyrbJzJZPnrtdcvmjrPmH3hClu7EuJZuJqB0MPJqCWrmCtfJYtQ5cE-rxs76GTaEOxM&usqp=CAU', cor: '#0398ce', corHEX: 0x0398ce },
+        { name: 'Floresta', id: '4', banner: 'https://img.freepik.com/fotos-premium/imagens-de-papel-de-parede-em-4k_655257-1108.jpg', cor: '#6628f6', corHEX: 0x6628f6 },
+        { name: 'Astronalta', id: '5', banner: 'https://res.cloudinary.com/dte7upwcr/image/upload/v1677788739/blog/blog2/ia-criar-imagens/ia-criar-imagens2.jpg', cor: '#01e4ca', corHEX: 0x01e4ca },
+        { name: 'Folha', id: '6', banner: 'https://img.freepik.com/fotos-gratis/beleza-abstrata-de-outono-em-padrao-multicolorido-de-veios-de-folhas-gerado-por-ia_188544-9871.jpg', cor: '#a20104', corHEX: 0xa20104 },
+        { name: 'Samael', id: '7', banner: 'https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/d6d9158f-1b03-4024-9f88-9d599c4c968a/df29tev-80fc62a5-5763-45a3-8b61-ec7f6d703924.png/v1/fit/w_600,h_240,q_70,strp/discord_banner__2__watermarked__by_gothymoth_df29tev-375w-2x.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9MjQwIiwicGF0aCI6IlwvZlwvZDZkOTE1OGYtMWIwMy00MDI0LTlmODgtOWQ1OTljNGM5NjhhXC9kZjI5dGV2LTgwZmM2MmE1LTU3NjMtNDVhMy04YjYxLWVjN2Y2ZDcwMzkyNC5wbmciLCJ3aWR0aCI6Ijw9NjAwIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmltYWdlLm9wZXJhdGlvbnMiXX0.PWzYbhsRZ8zU0Xn4y16vSFFyHg4SgbHE4pEw_O7_-LQ', cor: '#741111', corHEX: 0x741111 },
+        { name: 'Vaquinha', id: '8', banner: 'https://i.pinimg.com/1200x/ad/17/d5/ad17d516ba4254ead5cb9bd2747dcc53.jpg', cor: '#9600db', corHEX: 0x9600db },
+        { name: 'Imperfection', id: '9', banner: 'https://i.pinimg.com/originals/95/d0/3c/95d03cf844c7c024347258f8953236dd.gif', cor: '#db00a1', corHEX: 0xdb00a1 },
+        { name: 'Neblina', id: '10', banner: 'https://images-ext-1.discordapp.net/external/VqkxJ18-8oJKiLMoLUyz46VNBRb1XtCQjrFbJiLfqfo/https/wallpapers.com/images/hd/calm-aesthetic-desktop-8t7o1e3i0gaoodqz.jpg?format=webp&width=1258&height=683', cor: '#1f84ff', corHEX: 0x1f84ff },
+        { name: 'LOL', id: '11', banner: 'https://www.riotgames.com/darkroom/1440/056b96aab9c107bfb72c1cc818be712a:8e765b8b8b63d537b82096f248c2f169/tf-graves-pride-0.png', cor: '#f27900', corHEX: 0xf27900 },
+        { name: 'Skull Sea Of Thieves', id: '12', banner: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRgMl2_YrppPp5AgsNi9DHOjJkCFMDXPC55vQ&s', cor: '#6600ff', corHEX: 0x6600ff },
+        { name: 'Hajime no Ippo', id: '13', banner: 'https://m.media-amazon.com/images/S/pv-target-images/2e1f13308ead2fc251f71910b50e253af2f566d717f64dfcfd69a6ab5d8b00dd._SX1080_FMjpg_.jpg', cor: '#bf2126', corHEX: 0xbf2126 },
     ]
 
-    return banners 
+    return banners
 }
 
 
