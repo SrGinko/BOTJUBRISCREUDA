28be24c10c8735dfe32593e83db22e46f38104e0
SrGinko <pedeohenriquecardoso@gmail.com>
2025-07-25 05:36:39 -0300

    add: funcionalidade de para e limpar as músicas

---
+++ Commands/Musicas/stop.js
@@ -0,0 +1,20 @@
+const { SlashCommandBuilder, EmbedBuilder, MessageFlags } = require('discord.js')
+const { useQueue, useMainPlayer } = require('discord-player')
+const { addXp } = require('../../Controller')
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('stop')
+        .setDescription('Para a música e a fila de reprodução'),
+
+    async execute(interaction) {
+        const musicChannel = interaction.guild.channels.cache.find(ch => ch.id === '1053145878594068571')
+        const userId = interaction.member.id
+        const queue = useQueue(interaction.guild.id)
+
+        interaction.deferReply({ flags: [MessageFlags.Ephemeral] })
+
+        addXp(userId, 10)
+        queue.node.stop()
+    }
+}
