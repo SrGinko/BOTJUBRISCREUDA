280cd6ba7c5fff2403a911b8a88230cdf5f20b99
SrGinko <pedeohenriquecardoso@gmail.com>
2025-09-15 05:58:08 -0300

    refact: alteração em algumas funcionalidades

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const { Client, Events, GatewayIntentBits, Collection, ActivityType, Partials } = require('discord.js');
 const { Player } = require('discord-player');
-const { YoutubeiExtractor } = require('discord-player-youtubei');
 const { DefaultExtractors } = require('@discord-player/extractor');
 
 const dotenv = require('dotenv')
@@ -66,90 +65,7 @@ client.on('ready', async () => {
 	})
 	client.user.setStatus('online')
 	
-	await player.extractors.register(YoutubeiExtractor)
 	await player.extractors.loadMulti(DefaultExtractors)
-})
-
-client.on(Events.Raw, async (menssage) => {
-
-	if (!['MESSAGE_REACTION_ADD', 'MESSAGE_REACTION_REMOVE'].includes(menssage.t)) return
-	const { d: data } = menssage;
-	const guild = client.guilds.cache.get(data.guild_id)
-	if (!guild) return
-
-	const channel = guild.channels.cache.get(data.channel_id);
-	if (!channel) return console.log(channel)
-
-	const mensage1 = await channel.messages.fetch(data.messge_id)
-	if (!mensage1) return console.log(mensage1)
-
-	const member = await guild.members.fetch(data.user_id)
-	if (!member) return console.log(member)
-
-	const emoji = data.emoji.name
-
-	const cargoCorVermelha = guild.roles.cache.find(r => r.name === 'Vermelho')
-	const cargoCorLaranja = guild.roles.cache.find(r => r.name === 'Laranja')
-	const cargoCorAmarelo = guild.roles.cache.find(r => r.name === 'Amarelo')
-	const cargoCorVerde = guild.roles.cache.find(r => r.name === 'Verde')
-	const cargoCorAzul = guild.roles.cache.find(r => r.name === 'Azul')
-	const cargoCorRoxo = guild.roles.cache.find(r => r.name === 'Roxo')
-
-	if (channel.id == '1293772024518414356') {
-		if (menssage.t === 'MESSAGE_REACTION_ADD') {
-			switch (emoji) {
-				case '🔴':
-					await member.roles.add(cargoCorVermelha)
-					break;
-				case '🟠':
-					await member.roles.add(cargoCorLaranja)
-					break;
-				case '🟡':
-					await member.roles.add(cargoCorAmarelo)
-					break;
-				case '🟢':
-					await member.roles.add(cargoCorVerde)
-					break;
-				case '🔵':
-					await member.roles.add(cargoCorAzul)
-					break;
-				case '🟣':
-					await member.roles.add(cargoCorRoxo)
-					break;
-				default:
-					break;
-			}
-		} else if (menssage.t === 'MESSAGE_REACTION_REMOVE') {
-			switch (emoji) {
-				case '🔴':
-					await member.roles.remove(cargoCorVermelha)
-					break;
-				case '🟠':
-					await member.roles.remove(cargoCorLaranja)
-					break;
-				case '🟡':
-					await member.roles.remove(cargoCorAmarelo)
-					break;
-				case '🟢':
-					await member.roles.remove(cargoCorVerde)
-					break;
-				case '🔵':
-					await member.roles.remove(cargoCorAzul)
-					break;
-				case '🟣':
-					await member.roles.remove(cargoCorRoxo)
-					break;
-				default:
-					break;
-			}
-		}
-	} return
-})
-
-
-client.on(Events.InteractionCreate, interaction => {
-	if (!interaction.isChatInputCommand()) return;
-	console.log(interaction)
 })
 
 client.login(TOKEN);
