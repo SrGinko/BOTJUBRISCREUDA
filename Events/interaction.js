3d1ac7483b43d775ee7ac4e2b31683f45833b5b1
SrGinko <pedeohenriquecardoso@gmail.com>
2025-10-02 11:21:39 -0300

    add: adcionando o handle dos botões de interação das batalhas

--- Events/interaction.js
+++ Events/interaction.js
@@ -1,5 +1,11 @@
 const { Events } = require('discord.js')
 const { controler } = require('../Controller')
+const { obterItens } = require('../Utils/itensInventario')
+const chalk = require("chalk")
+const { handleAction } = require('../RPG/battleManager')
+
+const erro = chalk.bold.red
+
 
 module.exports = {
 	name: Events.InteractionCreate,
@@ -7,15 +13,50 @@ module.exports = {
 
 		controler(interaction)
 
+		if (interaction.isButton()) {
+			const res = await handleAction(interaction.customId, interaction.user)
+
+			if (!res.ok) {
+				return interaction.reply({ content: res.message, ephemeral: true })
+			}
+
+			if (!interaction.replied && !interaction.deferred) {
+				await interaction.deferUpdate()
+			}
+		}
+
+
+		if (interaction.isAutocomplete()) {
+
+			if (interaction.commandName === 'infoitem') {
+				const query = interaction.options.getFocused()
+
+				const itens = await obterItens()
+
+				const itensFilter = itens.filter(item => {
+					return item.nome.toLowerCase().includes(query.toLowerCase())
+				});
+
+				const selections = itensFilter.slice(0, 10).map(item => ({
+					name: item.nome,
+					value: String(item.id)
+				}));
+
+				await interaction.respond(selections);
+			}
+
+			return
+		}
+
 		if (!interaction.isChatInputCommand()) return;
 		const command = interaction.client.commands.get(interaction.commandName)
 
 		if (!command) {
-			console.error(`Comando ${interaction.commandName} não encontrado`)
+			console.error(erro(`Comando ${interaction.commandName} não encontrado`))
 		}
 		try {
 			await command.execute(interaction)
-		} catch(error) {
+		} catch (error) {
 			console.log(error)
 		}
 	}
