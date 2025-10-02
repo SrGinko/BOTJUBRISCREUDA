ade0cffc64e8010b4c9854c25256214d7d991de9
SrGinko <pedeohenriquecardoso@gmail.com>
2025-10-02 11:21:07 -0300

    add: adicionando emojis como icones de interface

---
+++ data/emojis.js
@@ -0,0 +1,17 @@
+const emojisData = {
+
+    inteface: {
+        estatisticas: "https://github.com/SrGinko/API-Jubscreuda/blob/RPG-System/src/Images/Icons/estatisitcas.png?raw=true",
+        mensagens: "https://github.com/SrGinko/API-Jubscreuda/blob/RPG-System/src/Images/Icons/mensagem2.png?raw=true",
+        foto: "https://github.com/SrGinko/API-Jubscreuda/blob/RPG-System/src/Images/Icons/foto.png?raw=true",
+        data: "https://github.com/SrGinko/API-Jubscreuda/blob/RPG-System/src/Images/Icons/data.png?raw=true",
+        conquista: "https://github.com/SrGinko/API-Jubscreuda/blob/RPG-System/src/Images/Icons/conquista.png?raw=true",
+        download: "https://github.com/SrGinko/API-Jubscreuda/blob/RPG-System/src/Images/Icons/download.png?raw=true",
+    },
+    rpg: {
+        moedas: "https://github.com/SrGinko/API-Jubscreuda/blob/RPG-System/src/Images/Icons/moedas.png?raw=true",
+    }
+
+}
+
+module.exports = emojisData
