47a896bd1dbba1c7de2df23eb3de5c5b03d76019
SrGinko <pedeohenriquecardoso@gmail.com>
2026-02-10 13:17:29 -0300

    adiciona funcionalidade para criar mensagens com modal interativo

--- Commands/Comandos/criarMessage.js
+++ Commands/Comandos/criarMessage.js
@@ -1,4 +1,5 @@
-const { SlashCommandBuilder, ModalBuilder, TextInputBuilder, ActionRowBuilder, TextInputStyle } = require('discord.js')
+const { SlashCommandBuilder, ModalBuilder, TextInputBuilder, ActionRowBuilder, TextInputStyle, ChannelType, TextDisplayBuilder, LabelBuilder, ComponentType, LabelAssertions } = require('discord.js')
+const { vault } = require('googleapis/build/src/apis/vault')
 
 
 module.exports = {
@@ -8,17 +9,67 @@ module.exports = {
 
     async execute(interaction) {
 
-        const canais = interaction.guild.channels.cache.filter(c => c.isTextBased())
+        try{
 
-        console.log(canais)
-
-        const modal = new ModalBuilder({
-            title: 'Enviar Mensagem',
-            customId: 'criarMessage',
-        })
+        if (interaction.user.id === '770818264691114016') {
+            
+            const channelTexts = interaction.guild.channels.cache.filter(c => c.type === ChannelType.GuildText)
+            
+            const modal = new ModalBuilder({
+                title: 'Enviar Mensagem',
+                custom_id: `system:criarmensagem:${interaction.user.id}`,
+            })
+            .addTextDisplayComponents(
+                new TextDisplayBuilder({
+                    content: 'Preencha os campos obrigatórios para Criar a mensagem'
+                })
+            )
+            .addLabelComponents(
+                new LabelBuilder({
+                    label: 'Titulo:',
+                    description: 'Insira o Titulo do container',
+                    component:{
+                        type: ComponentType.TextInput, 
+                        style: TextInputStyle.Short,
+                        custom_id: "titulo",
+                        required: false,
+                    }
+                })
+            )
+            .addLabelComponents(
+                new LabelBuilder({
+                    label: "Conteudo:",
+                    description: "Insira o conteúdo da mensagem",
+                    component:{
+                        type: ComponentType.TextInput,
+                        style: TextInputStyle.Paragraph,
+                        custom_id: 'conteudo',
+                        required: true
+                    }
+                })
+            )
+            .addLabelComponents(
+                new LabelBuilder({
+                    label: "Canal:",
+                    description: "Selecione qual canal de texto",
+                    component:{
+                        type: ComponentType.StringSelect,
+                        custom_id: 'canaltexto',
+                        required: true,
+                        options: channelTexts.map(ch => (
+                            {label: `${ch.name}`, value: `${ch.id}`}
+                        )).slice(0,25)
+                    }
+                })
+            )
         
-
-        // await interaction.showModal(modal)
+            await interaction.showModal(modal)
+        }else{
+            await interaction.reply({content: 'Apenas o meu criador pode usar esse comando!', ephemeral: true})
+        }
+    }catch(erro){
+        console.log(erro)
+    }
 
     }
 }
