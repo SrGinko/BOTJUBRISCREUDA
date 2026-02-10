f3c45edf9f711822f32c469120a14755bbf47cab
SrGinko <pedeohenriquecardoso@gmail.com>
2026-02-10 13:20:45 -0300

    refatora o gerenciamento de interações para utilizar novos manipuladores de ações e remove código obsoleto

--- Events/interaction.js
+++ Events/interaction.js
@@ -1,11 +1,9 @@
 const { Events } = require('discord.js')
-const { controler, handleAction } = require('../Controller')
-const { obterItens, obterUnicoItem } = require('../Utils/itensInventario')
+const { handleActionButton } = require('../handlers/buttonsHandler')
+const { obterItens } = require('../Utils/itensInventario')
 const chalk = require("chalk")
-const { apiTeste, api } = require('../Utils/axiosClient')
-const guildEvent = require('./GuildEvent')
-const { criarEmbed } = require('../Utils/embedFactory')
-const { updateBattleMessage, enemyTurn, getBattle } = require('../RPG/battleManager')
+const { ModalHandleAction } = require('../handlers/modalHandler')
+const { SelectMenusHandleAction } = require('../handlers/selectmenusHandler')
 const erro = chalk.bold.red
 
 
@@ -13,70 +11,17 @@ module.exports = {
 	name: Events.InteractionCreate,
 	async execute(interaction) {
 
-		controler(interaction)
-
 		if (interaction.isModalSubmit()) {
+			ModalHandleAction(interaction)
+		}
 
-			const channel = await interaction.guild.channels.fetch('1428656070552850483')
-
-			const [prefix, action, id] = interaction.customId.split(':')
-
-			if (prefix === 'tv') {
-				switch (action) {
-					case 'addcanal':
-
-						const nomeCanal = interaction.fields.getTextInputValue('canalNome')
-						const canalUrl = interaction.fields.getTextInputValue('canalUrl')
-						const capaCanal = interaction.fields.getTextInputValue('capaUrl')
-
-						api.post('/tv/canais', {
-							nome: nomeCanal,
-							url: canalUrl,
-							capaUrl: capaCanal
-						})
-						interaction.reply({
-							embeds: [
-								criarEmbed({
-									description: 'Encaminhado dados !',
-									color: 'Green'
-								})
-							], flags: 64
-						})
-						guildEvent.emit('addcanal', { nomeCanal: nomeCanal, CapaCanal: capaCanal, channel: channel })
-
-						break;
-
-					default:
-						break;
-				}
-			}if (prefix === 'rpg') {
-				switch (action) {
-					case 'curar':{
-						const selecionados = interaction.fields.getStringSelectValues('consumivelSelect')
-						const itensUsados = await Promise.all(selecionados.map(async (itemId) => {
-							const res = await obterUnicoItem(Number(itemId))
-							return res
-						}))
-						
-						const curarAmount = itensUsados.reduce((acc, item) => acc + (item.heal || 0), 0)
-						
-						const batalha = getBattle(id)
-
-						batalha.player.hp = Math.min(batalha.player.maxHp, batalha.player.hp + curarAmount)
-
-						await interaction.deferUpdate();
-
-						await updateBattleMessage(batalha, `Você usou itens e recuperou ${curarAmount} de vida!`)
-						await enemyTurn(batalha)
-
-					}
-				}
-			}
+		if(interaction.isStringSelectMenu()) {
+			SelectMenusHandleAction(interaction)
 		}
 
 		if (interaction.isButton()) {
 
-			const res = await handleAction(interaction.customId, interaction.user, interaction)
+			const res = await handleActionButton(interaction.customId, interaction.user, interaction)
 
 			if (!res.ok) {
 				return interaction.reply({ content: res.message, ephemeral: true })
@@ -90,7 +35,7 @@ module.exports = {
 
 		if (interaction.isAutocomplete()) {
 
-			if (interaction.commandName === 'infoitem') {
+			if (interaction.commandName === 'loja') {
 				const query = interaction.options.getFocused()
 
 				const itens = await obterItens()
