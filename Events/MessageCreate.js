69c967f0b83bcf45b572ea9566cb813446ebcf00
SrGinko <pedeohenriquecardoso@gmail.com>
2026-03-20 11:02:04 -0300

    remove lógica de conquistas e papéis do usuário na função de criação de mensagens

--- Events/MessageCreate.js
+++ Events/MessageCreate.js
@@ -32,12 +32,6 @@ module.exports = {
             return
         }
 
-        const faladorBronze = message.guild.roles.cache.find(r => r.name === 'Falador Bronze')
-        const faladorPrata = message.guild.roles.cache.find(r => r.name === 'Falador Prata')
-        const faladorOuro = message.guild.roles.cache.find(r => r.name === 'Falador Ouro')
-        const faladorPlatina = message.guild.roles.cache.find(r => r.name === 'Falador Platina')
-        const faladorDiamante = message.guild.roles.cache.find(r => r.name === 'Falador Diamante')
-
         const bot = message.guild.members.cache.get(message.author.id)
         const cargo = '1286201893516742696'
         if (message.channel.id !== '1053145878594068571') {
@@ -50,51 +44,8 @@ module.exports = {
 
         if (!message.author.bot) {
 
-            const mensagemTotal = await addMenssage(message.author.id, 1)
+            await addMenssage(message.author, 1, message)
             addXp(message.author.id, 10)
-
-            switch (mensagemTotal) {
-                case 500:
-                    guildEvent.emit('conquista', { conquista: faladorBronze, xp: 500, user: message.author, channel: message.channel })
-                    addXp(message.author.id, 500)
-                    message.member.roles.add(faladorBronze).catch(console.error)
-                    break;
-                case 1500:
-                    guildEvent.emit('conquista', { conquista: faladorPrata, xp: 1500, user: message.author, channel: message.channel })
-                    addXp(message.author.id, 1500)
-                    message.member.roles.add(faladorPrata).catch(console.error)
-                    if (message.member.roles.cache.has(faladorBronze)) {
-                        message.member.roles.remove(faladorBronze)
-                    }
-                    message.member.roles.remove(faladorBronze).catch(console.error)
-                    break;
-                case 3000:
-                    guildEvent.emit('conquista', { conquista: faladorOuro, xp: 3000, user: message.author, channel: message.channel })
-                    addXp(message.author.id, 3000)
-                    message.member.roles.add(faladorOuro).catch(console.error)
-                    if (message.member.roles.cache.has(faladorPrata)) {
-                        message.member.roles.remove(faladorPrata)
-                    }
-                    break;
-                case 5000:
-                    guildEvent.emit('conquista', { conquista: faladorPlatina, xp: 5000, user: message.author, channel: message.channel })
-                    addXp(message.author.id, 5000)
-                    message.member.roles.add(faladorPlatina).catch(console.error)
-                    if (message.member.roles.cache.has(faladorOuro)) {
-                        message.member.roles.remove(faladorOuro)
-                    }
-                    break;
-                case 10000:
-                    guildEvent.emit('conquista', { conquista: faladorDiamante, xp: 10000, user: message.author, channel: message.channel })
-                    addXp(message.author.id, 10000)
-                    message.member.roles.add(faladorDiamante).catch(console.error)
-                    if (message.member.roles.cache.has(faladorPlatina)) {
-                        message.member.roles.remove(faladorPlatina)
-                    }
-                    break;
-                default:
-                    break;
-            }
 
         }
 
