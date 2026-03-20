ccfc46592baf81c541df1c9ef0adeb3fb07ff7ed
SrGinko <pedeohenriquecardoso@gmail.com>
2026-03-20 11:02:12 -0300

    adiciona documentação para a função de tratamento de erros

--- handlers/errorsHandler.js
+++ handlers/errorsHandler.js
@@ -1,5 +1,11 @@
 const { ContainerBuilder, MessageFlags } = require("discord.js");
 
+/***
+ * @description Função para lidar com erros de forma padronizada
+ * @param {Interaction} interaction - A interação do Discord que gerou o erro
+ * @param {String} mensagem - A mensagem de erro a ser exibida para o usuário
+ * @param {String} erro - O título do erro a ser exibido no container
+ */
 async function handleError(interaction, mensagem, erro){
     const container = new ContainerBuilder({
         accent_color: 0xff0000,
@@ -13,3 +19,5 @@ async function handleError(interaction, mensagem, erro){
     await interaction.reply({ components: [container], flags: [MessageFlags.Ephemeral, MessageFlags.IsComponentsV2] })
 
 }
+
+module.exports ={ handleError}
