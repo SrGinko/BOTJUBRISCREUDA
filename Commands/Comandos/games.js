b09010b07e3046f320a33493a9f33fc11df063e9
SrGinko <pedeohenriquecardoso@gmail.com>
2026-05-31 07:38:51 -0300

    atualizado comando 'games' para buscar informações detalhadas do jogo e exibir na interface

--- Commands/Comandos/games.js
+++ Commands/Comandos/games.js
@@ -1,55 +1,81 @@
-const { SlashCommandBuilder, StringSelectMenuBuilder, ContainerBuilder, TextDisplayBuilder, ActionRowBuilder, MessageFlags } = require("discord.js")
-const { BuscarjogoNome } = require("../../Utils/buscarJogos")
+const { SlashCommandBuilder, StringSelectMenuBuilder, ContainerBuilder, TextDisplayBuilder, ButtonStyle, ActionRowBuilder, MessageFlags, MediaGalleryBuilder, SeparatorBuilder, SeparatorSpacingSize, ButtonBuilder } = require("discord.js")
+const { BuscarjogoId } = require("../../Utils/buscarJogos")
 const { addXp } = require("../../Utils/xp")
+const { ConversorHtmltoText } = require("../../Utils/ConversorHtmltoText")
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('games')
         .setDescription('Busca algumas informações de um determinado jogo')
-        .addStringOption(option => option.setName('game').setDescription('Insira o nome do jogo').setRequired(true)),
+        .addStringOption(option => option.setName('game').setDescription('Insira o nome do jogo').setAutocomplete(true).setRequired(true)),
 
     async execute(interaction) {
         await interaction.deferReply()
 
-        const { options } = interaction
-        const userId = interaction.user.id
+        const appId = interaction.options.getString('game');
 
-        const nameGame = options.getString('game')
+        const jogo = await BuscarjogoId(appId)
 
-        try {
+        console.log(jogo)
 
-            const response = await BuscarjogoNome(nameGame)
+        const container = new ContainerBuilder()
 
-            if (response.length > 0) {
-                const row = new StringSelectMenuBuilder()
-                    .setCustomId('game')
-                    .setPlaceholder(`Selecione o jogo`)
-                    .addOptions(response.slice(0, 25).map(game => ({
-                        label: game.name,
-                        value: `${game.name}`,
-                        description: game.plataform,
-                        emoji: '<:controle:1463846749285257260>'
-                    })))
+        container.addMediaGalleryComponents(
+            new MediaGalleryBuilder({
+                items: [
+                    { media: { url: jogo.background_raw } }
+                ]
+            })
+        )
 
-                const container = new ContainerBuilder({
-                    accent_color: 0x3c1099,
-                    components: [
-                        new TextDisplayBuilder({
-                            content: `## <:controle:1463846749285257260> Selecione o jogo desejado`,
-                            style: 'Short',
-                        }),
+        container.addSeparatorComponents(
+            new SeparatorBuilder({
+                spacing: SeparatorSpacingSize.Large,
+                divider: false
+            })
+        )
 
-                        new ActionRowBuilder()
-                            .addComponents(row)
-                    ]
-                })
-                addXp(userId, 10)
-                await interaction.editReply({ flags: [MessageFlags.IsComponentsV2], components: [container] })
-            }
+        container.addTextDisplayComponents(
+            new TextDisplayBuilder({
+                content:
+`# ${jogo.name}
 
-        } catch (error) {
-            console.log(error)
+<:moedas:1463846758282170521> ${jogo.is_free
+    ? 'Gratuito'
+    : jogo.price_overview?.final_formatted || 'Desconhecido'}
+
+**Metacritic:** ${jogo.metacritic ? `[${jogo.metacritic.score}](${jogo.metacritic.url})` : 'Desconecido'}
+${jogo.website ? `[Website](${jogo.website})` : 'Desconecido'}
+
+> ${ConversorHtmltoText(jogo.short_description) || 'Nenhuma descrição disponível'}
+
+**Lançamento:** ${
+    jogo.release_date.coming_soon
+        ? 'Em breve'
+        : jogo.release_date.date || 'Desconhecido'
+}
+**Gêneros:** ${
+    jogo.genres?.map(g => g.description).join(', ') || 'Não informado'
+}
+**Categorias:** ${
+    jogo.categories?.slice(0, 10).map(c => c.description).join(', ') || 'Não informado'
+}`
+            })
+        )
+
+        container.addActionRowComponents(
+            new ActionRowBuilder().addComponents(
+                new ButtonBuilder().setEmoji(`<:foto:1463846754322747497>`).setLabel('Imagens').setStyle(ButtonStyle.Secondary).setCustomId(`games:imagens:${appId}`),
+                new ButtonBuilder().setEmoji('<:estatisitcas:1463846753110331537>').setLabel('Requisitos').setStyle(ButtonStyle.Secondary).setCustomId(`games:requisitos:${appId}`),
+                new ButtonBuilder().setEmoji('➕').setLabel('Adicionar Sugestão').setStyle(ButtonStyle.Secondary).setCustomId(`games:suggest:${appId}`),
+                new ButtonBuilder().setEmoji('<:moedas:1463846758282170521>').setLabel('Comprar').setStyle(ButtonStyle.Link).setURL(`https://store.steampowered.com/app/${appId}`)
+            )
+        )
+
+        if (!jogo || jogo.length === 0) {
+            return interaction.editReply({ content: 'Nenhum jogo encontrado com esse nome.', flags: MessageFlags.Ephemeral })
         }
-
+        addXp(interaction.user.id, 10)
+        interaction.editReply({ components: [container], flags: [MessageFlags.IsComponentsV2] })
     }
 }
